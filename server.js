503ef972766b5f4e3724849a125e09b2d780f3b5
jswalker808 <seiyawalk@gmail.com>
2018-05-21 14:45:42 -0600

    Changed the port

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.put('/api/', (req, res) => {
   res.send(highScore.toString());
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'))
+app.listen(3002, () => console.log('Server listening on port 3000!'))
